b45342474f4d4b74faf7c930323bc3f6071dc493
kcWu <79691025+kcwu229@users.noreply.github.com>
2022-01-21 19:14:54 +0800

    Add files via upload

---
+++ ExerciseForLibrary2/src/main/java/com/javatpoint/beans/Book.java
@@ -0,0 +1,49 @@
+package com.javatpoint.beans;    
+    
+public class Book {    
+	private int id;
+	private String title;
+	private String author;
+	private int year;
+	private String publisher;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	public String getPublisher() {
+		return publisher;
+	}
+	
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+}

be2f078cac71ef18fe316f60889b631c24a6489c
kcwu229 <ggds47uh@protonmail.com>
2024-11-18 02:08:20 +0800

    updated the homepage and bookpage (part 2)

--- samLibrary_fullstack/frontend/src/components/bookFilter/BookFilterData.jsx
+++ samLibrary_fullstack/frontend/src/components/bookFilter/BookFilterData.jsx
@@ -14,4 +14,77 @@ const languages = [
   { id: "other", label: "Other" },
 ];
 
-export { categories, languages };
+const exampleBooks = [
+  {
+    id: 1,
+    title: "Book One",
+    author: "Author One",
+    rating: 4.5,
+    remainingCount: 10,
+  },
+  {
+    id: 2,
+    title: "Book Two",
+    author: "Author Two",
+    rating: 4.0,
+    remainingCount: 5,
+  },
+  {
+    id: 3,
+    title: "Book Three",
+    author: "Author Three",
+    rating: 3.5,
+    remainingCount: 8,
+  },
+  {
+    id: 4,
+    title: "Book Four",
+    author: "Author Four",
+    rating: 5.0,
+    remainingCount: 2,
+  },
+  {
+    id: 5,
+    title: "Book Five",
+    author: "Author Five",
+    rating: 4.2,
+    remainingCount: 7,
+  },
+  {
+    id: 6,
+    title: "Book Six",
+    author: "Author Six",
+    rating: 3.8,
+    remainingCount: 12,
+  },
+  {
+    id: 7,
+    title: "Book Seven",
+    author: "Author Seven",
+    rating: 4.7,
+    remainingCount: 3,
+  },
+  {
+    id: 8,
+    title: "Book Eight",
+    author: "Author Eight",
+    rating: 4.1,
+    remainingCount: 6,
+  },
+  {
+    id: 9,
+    title: "Book Nine",
+    author: "Author Nine",
+    rating: 3.9,
+    remainingCount: 9,
+  },
+  {
+    id: 10,
+    title: "Book Ten",
+    author: "Author Ten",
+    rating: 4.3,
+    remainingCount: 4,
+  },
+];
+
+export { categories, languages, exampleBooks };
